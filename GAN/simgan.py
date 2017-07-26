bc5cf85c3d45c249b75e6c81484693c9ea6de639
Unknown <cmxu@comcast.net>
2017-07-26 01:10:40 -0400

    changed fetch captchas

---
+++ GAN/simgan.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+dir = "../data_gathering/captchas/"
+
+imgpath = dir + "c0.png"
+
+im = Image.open(imgpath)
+
+
+
--- data_gathering/fetchNumCaptchas.sh
+++ data_gathering/fetchNumCaptchas.sh
@@ -5,9 +5,15 @@ if [ ! -d "$captchas" ]; then
     mkdir captchas
 fi
 
-for((i=1;i<=$1;i+=1));
+for((i=0;i<$1;i+=1));
 do
-    echo $i
-    c='c'$i'.png'
+    #echo $i
+    if [ $i -lt 10 ]; then
+	c='c00'$i'.png'
+    elif [ $i -lt 100 ]; then
+	c='c0'$i'.png'
+    else
+	c='c'$i'.png'
+    fi
     wget -q https://projecteuler.net/captcha/show_captcha.php -O captchas/$c &
 done
