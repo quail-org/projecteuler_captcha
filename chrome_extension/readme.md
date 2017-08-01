9f13efff8705959ac50071ef1c346b04b361fa0d
Andrew Zhang <alz2@illinois.edu>
2017-07-31 23:38:05 -0400

    grab image on page

--- chrome_extension/readme.md
+++ chrome_extension/readme.md
@@ -1,1 +1,1 @@
-
+[Helpful link](https://stackoverflow.com/questions/934012/get-image-data-in-javascript)
--- chrome_extension/src/inject/inject.js
+++ chrome_extension/src/inject/inject.js
@@ -7,7 +7,19 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+        
+        //https://stackoverflow.com/questions/934012/get-image-data-in-javascript
+        var captcha = document.getElementById("captcha_image");
 
-	}
+        var canvas = document.createElement("canvas");
+        canvas.width = captcha.width;
+        canvas.height = captcha.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(captcha, 0, 0);
+        
+        var dataURL = canvas.toDataURL("image/png");
+        console.log(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+    }
 	}, 10);
 });
